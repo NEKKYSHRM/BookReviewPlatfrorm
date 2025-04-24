dd43026e1c82bcec07e37746007be5ed882cb0a4
Nikhil Kumar Sharma <nikhilkumarsharman815@gmail.com>
2025-04-24 11:09:37 +0530

    Setup Project Structures

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div className='font-bold text-green-500'>Hello</div>
+  )
+}
+
+export default App
